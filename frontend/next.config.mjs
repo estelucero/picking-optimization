01cc49d8d2e8b4d7a996b3aa4ef6a61130370fb9
Franco Disabato <francuster1@gmail.com>
2026-05-15 16:16:52 -0300

    front end debug

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+
+  productionBrowserSourceMaps: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
